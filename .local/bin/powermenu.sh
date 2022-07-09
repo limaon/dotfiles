f9d9b828e8820db93c9a6b3ed31731244754f313
Alvaro Oliveira <51033564+limaon@users.noreply.github.com>
2022-07-09 11:28:15 -0400

    Sets for dmenu

--- .local/bin/powermenu.sh
+++ .local/bin/powermenu.sh
@@ -1,17 +1,21 @@
 #!/usr/bin/env bash
 
 function powermenu {
-    options="Cancel\nShutdown\nReboot\nSuspend\nLock"
-    selected=$(echo -e $options | dmenu) 
+    options="Cancel\nShutdown\nReboot\nSuspend\nLock\nLogout"
+    selected=$(echo -e $options | dmenu -i -p "Select: ") 
     if [[ $selected = "Shutdown" ]]; then
-        poweroff
+        systemctl poweroff -i
     elif [[ $selected = "Reboot" ]]; then
-        reboot
+        systemctl reboot
     elif [[ $selected = "Suspend" ]]; then
        systemctl suspend 
     elif [[ $selected = "Lock" ]]; then
-        betterlockscreen --lock dimblur
+        dm-tool lock
+    elif [[ $selected = "Logout" ]]; then
+        i3-msg exit
     elif [[ $selected = "Cancel" ]]; then
         return
     fi
 }
+
+powermenu
