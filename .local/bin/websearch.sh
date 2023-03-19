f4d5d903422f7b7d7f7450b026cf9dc76904dc72
Alvaro Oliveira <51033564+limaon@users.noreply.github.com>
2023-03-18 20:07:50 -0400

    rebuild script to show only name

--- .local/bin/websearch.sh
+++ .local/bin/websearch.sh
@@ -1,53 +1,67 @@
 #!/usr/bin/env bash
-#
-# Script name: dmsearch
-# Description: Search various search engines (inspired by surfraw).
-# Dependencies: dmenu and a web browser
-# GitLab: https://www.gitlab.com/dwt1/dmscripts
-# Contributors: Derek Taylor
 
-# Defining our web browser.
-# DMBROWSER="firefox"
-DMBROWSER="xdg-open"
+readonly BROWSER=${BROWSER:-xdg-open}
+FONT="TerminessTTF Nerd Font:size=12"
 
-# An array of search engines.  You can edit this list to add/remove
-# search engines. The format must be: "engine_name - url".
-# The url format must allow for the search keywords at the end of the url.
-# For example: https://www.amazon.com/s?k=XXXX searches Amazon for 'XXXX'.
+# Definindo opções
 declare -a options=(
-" Google - https://www.google.com/search?q="
-" Map - https://www.google.com/maps/search/"
-" Youtube - https://www.youtube.com/results?search_query="
-" Twitter - https://twitter.com/search?q="
-" Googleimages - https://www.google.com/search?hl=en&tbm=isch&q="
-" Duckduckgo - https://duckduckgo.com/?q="
-" Github - https://github.com/search?q="
-" Gitlab - https://gitlab.com/search?search="
-" Letterboxd - https://letterboxd.com/search/"
-" Trans https://www.reverso.net/tradu%C3%A7%C3%A3o-texto#sl=eng&tl=por&text="
-" Context - https://context.reverso.net/traducao/ingles-portugues/"
-" Wikipedia - https://en.wikipedia.org/wiki/"
-" Aur - https://aur.archlinux.org/packages?O=0&SeB=nd&K="
-" Dic https://www.collinsdictionary.com/dictionary/english/"
-" Youglish - https://youglish.com/pronounce/"
-" Tatoeba - https://tatoeba.org/pt-br/sentences/search?from=eng&to=por&query="
-" Cm - https://codemadness.org/idiotbox/?q="
-" Yy - https://yewtu.be/search?q="
-" Udemy - https://www.udemy.com/courses/search/?src=ukw&q="
-"quit"
+  "Google"
+  "Map"
+  "DuckDuckGo"
+  "Bing"
+  "Yahoo"
+  "Youtube"
+  "Translate"
+  "Twitter" 
+  "Googleimages"
+  "Github"
+  "Gitlab"
+  "Letterboxd"
+  "Context"
+  "Wikipedia"
+  "Aur"
+  "Dictionary"
+  "Youglish"
+  "Tatoeba"
+  "Cm"
+  "Yy"
+  "Udemy"
 )
 
-# Picking a search engine.
-while [ -z "$engine" ]; do
-enginelist=$(printf '%s\n' "${options[@]}" | dmenu -i -l 4 -fn "TerminessTTF Nerd Font" -p ' WebSearch ') || exit
-engineurl=$(echo "$enginelist" | awk '{print $NF}')
-engine=$(echo "$enginelist" | awk '{print $1}')
-done
+# Obtendo seleção do usuário
+choice=$(printf '%s\n' "${options[@]}" | sort -u | dmenu -i -fn "$FONT" -p " WebSearch:")
 
-# Searching the chosen engine.
-while [ -z "$query" ]; do
-  query=$( $engine | dmenu -i -fn "TerminessTTF Nerd Font" -p "Searching in $engine") || exit
-done
+# Definindo a URL de pesquisa para cada opção
+case "$choice" in
+  "Google") search_url="https://www.google.com/search?q=";;
+  "DuckDuckGo") search_url="https://duckduckgo.com/?q=";;
+  "Bing") search_url="https://www.bing.com/search?q=";;
+  "Yahoo") search_url="https://search.yahoo.com/search?p=";;
+  "Map") search_url="https://www.google.com/maps/search/";;
+  "Youtube") search_url="https://www.youtube.com/results?search_query=";;
+  "Translate") search_url="https://www.reverso.net/tradu%C3%A7%C3%A3o-texto#sl=eng&tl=por&text=";;
+  "Twitter") search_url="- https://twitter.com/search?q=";;
+  "Googleimages") search_url="https://www.google.com/search?hl=en&tbm=isch&q=";;
+  "Github") search_url="https://github.com/search?q=";;
+  "Gitlab") search_url="https://gitlab.com/search?search=";;
+  "Letterboxd") search_url="https://letterboxd.com/search/";;
+  "Context") search_url="https://context.reverso.net/traducao/ingles-portugues/";;
+  "Wikipedia") search_url="https://en.wikipedia.org/wiki/";;
+  "Aur") search_url="https://aur.archlinux.org/packages?O=0&SeB=nd&K=";;
+  "Dic") search_url="https://www.collinsdictionary.com/dictionary/english/";;
+  "Youglish") search_url="https://youglish.com/pronounce/";;
+  "Tatoeba") search_url="https://tatoeba.org/pt-br/sentences/search?from=eng&to=por&query=";;
+  "Cm") search_url="https://codemadness.org/idiotbox/?q=";;
+  "Yy") search_url="https://yewtu.be/search?q=";;
+  "Udemy") search_url="https://www.udemy.com/courses/search/?src=ukw&q=";;
+  *) exit 1;;
+esac
 
-# Display search results in web browser
-$DMBROWSER "$engineurl""$query"
+# Obtendo a pesquisa do usuário
+search=$(echo "<= Type here" | dmenu -i -fn "$FONT" -p "Searching in $choice:")
+
+# Verificando se o usuário entrou com um valor para a pesquisa
+if [ -n "$search" ]; then
+  # Abrindo o navegador padrão com a URL de pesquisa e o termo de pesquisa
+  $BROWSER "$search_url$search"
+fi
