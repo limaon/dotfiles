aed669bdc0a208d997422536a7dc50297f10c3af
Alvaro Oliveira <51033564+limaon@users.noreply.github.com>
2024-11-01 16:11:21 -0400

    Add new scripts and update configuration file

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -5,6 +5,7 @@ from qutebrowser.config.config import ConfigContainer
 from urllib.parse import urljoin
 import operator
 import re
+
 config: ConfigAPI = config
 c: ConfigContainer = c
 dowmload_open_script_path = "/usr/share/qutebrowser/userscripts/open_download"
@@ -13,7 +14,7 @@ config.load_autoconfig(False)
 
 # REDIRECT CONFIG
 def _debian_redir(url: QUrl) -> bool:
-    p = url.path().strip('/')
+    p = url.path().strip("/")
     if p.isdigit():
         url.setPath(urljoin("/plain/", p))
         return True
@@ -21,16 +22,16 @@ def _debian_redir(url: QUrl) -> bool:
 
 
 def _the_compiler_redir(url: QUrl) -> bool:
-    p = url.path().strip('/')
+    p = url.path().strip("/")
     res = re.search(r"\w+$", p)
-    if p.startswith('view/') and res:
+    if p.startswith("view/") and res:
         url.setPath(urljoin("/view/raw/", res.group()))
         return True
     return False
 
 
 def _pastebin_redir(url: QUrl) -> bool:
-    p = url.path().strip('/')
+    p = url.path().strip("/")
     if p.isalnum():
         url.setPath(urljoin("/raw/", p))
         return True
@@ -38,7 +39,7 @@ def _pastebin_redir(url: QUrl) -> bool:
 
 
 def _hastebin_redir(url: QUrl) -> bool:
-    p = url.path().strip('/')
+    p = url.path().strip("/")
     if "raw" not in p:
         url.setPath(urljoin("/raw/", p))
         return True
@@ -48,21 +49,16 @@ def _hastebin_redir(url: QUrl) -> bool:
 # Any return value other than a literal 'False' means we redirected
 # type: typing.Dict[str, typing.Callable[..., typing.Optional[bool]]]
 REDIRECT_MAP = {
-    "reddit.com": operator.methodcaller('setHost', 'libreddit.de'),
-    "www.reddit.com": operator.methodcaller('setHost', 'libreddit.de'),
-    "twitter.com": operator.methodcaller('setHost', 'mobile.twitter.com'),
-    "www.twitter.com": operator.methodcaller('setHost', 'mobile.twitter.com'),
+    "reddit.com": operator.methodcaller("setHost", "reddit.rtrace.io"),
+    "www.reddit.com": operator.methodcaller("setHost", "reddit.rtrace.io"),
+    "twitter.com": operator.methodcaller("setHost", "mobile.twitter.com"),
+    "www.twitter.com": operator.methodcaller("setHost", "mobile.twitter.com"),
+    "medium.com": operator.methodcaller("setHost", "md.vern.cc"),
+    "www.medium.com": operator.methodcaller("setHost", "md.vern.cc"),
     # "youtube.com": operator.methodcaller('setHost', 'piped.video'),
     # "www.youtube.com": operator.methodcaller('setHost', 'piped.video'),
-    "en.wikipedia.org": operator.methodcaller('setHost', 'wikiless.org'),
-    "www.en.wikipedia.org": operator.methodcaller('setHost', 'wikiless.org'),
-    "medium.com": operator.methodcaller('setHost', 'scribe.rip'),
-    "www.medium.com": operator.methodcaller('setHost', 'scribe.rip'),
-
-
-    "pt.wikipedia.org": operator.methodcaller('setHost', 'wikiless.org'),
-    "www.pt.wikipedia.org": operator.methodcaller('setHost', 'wikiless.org'),
-
+    # "en.wikipedia.org": operator.methodcaller("setHost", "wikiless.org"),
+    # "www.en.wikipedia.org": operator.methodcaller("setHost", "wikiless.org"),
     # Pastebins
     "paste.debian.net": _debian_redir,
     "paste.the-compiler.org": _the_compiler_redir,
@@ -75,8 +71,10 @@ REDIRECT_MAP = {
 
 def int_fn(info: interceptor.Request):
     """Block the given request if necessary."""
-    if (info.resource_type != interceptor.ResourceType.main_frame or
-            info.request_url.scheme() in {"data", "blob"}):
+    if (
+        info.resource_type != interceptor.ResourceType.main_frame
+        or info.request_url.scheme() in {"data", "blob"}
+    ):
         return
     url = info.request_url
 
@@ -92,9 +90,9 @@ interceptor.register(int_fn)
 # UI
 c.fonts.default_family = [
     "Terminus",
-    "Ubuntu Nerd Font",
+    "Ubuntu Mono",
     "Noto Sans CJK JP",
-    "Noto Color Emoji"
+    "Noto Color Emoji",
 ]
 c.fonts.default_size = "16px"
 c.downloads.remove_finished = 10000
@@ -118,44 +116,56 @@ c.url.default_page = "https://duckduckgo.com/"
 c.url.start_pages = ["https://duckduckgo.com/"]
 
 # GENERAL
-c.input.insert_mode.auto_load = True
+c.input.insert_mode.auto_load = False
 c.content.default_encoding = "utf-8"
 c.auto_save.session = False
 c.content.prefers_reduced_motion = True
 c.content.xss_auditing = True
 c.content.javascript.modal_dialog = True
-c.content.javascript.clipboard = "access"
+c.content.javascript.clipboard = "access-paste"
 c.content.notifications.enabled = "ask"
-c.content.notifications.presenter = "libnotify"
+c.content.notifications.presenter = "auto"
 c.content.notifications.show_origin = True
 c.content.javascript.can_open_tabs_automatically = True
 c.downloads.location.prompt = False
 c.content.autoplay = False
-c.content.plugins = True
+c.content.geolocation = False
+c.content.plugins = False
 c.content.pdfjs = False
+c.content.images = True
 c.downloads.location.directory = "~/Downloads/"
-c.content.canvas_reading = False
+c.content.canvas_reading = True
 c.content.webgl = False
 c.qt.highdpi = True
+c.content.blocking.method = "auto"
 c.downloads.open_dispatcher = dowmload_open_script_path
-c.editor.command = ["kitty", "nvim", "-e", "exec {line}g{column0}l", "{}"]
+c.editor.command = ["tmux", "new-window", "nvim {}"]
 c.qt.force_software_rendering = "chromium"
+c.qt.chromium.process_model = "process-per-site"
+c.qt.chromium.low_end_device_mode = "always"
 c.qt.args += [
-    "ignore-gpu-blacklist",
+    "disable-gpu",
+    "disable-accelerated-video-decode",
+    "disable-accelerated-fullscreen",
+    "disable-experimental-canvas-features",
+    "disable-multi-monitor-painting",
+    "disable-gpu-compositing",
+    "disable-accelerated-video",
     "--widevine",
 ]
 
 # Keybinds
-config.unbind('m')
+config.unbind("m")
 bindings = {
-    'M': 'hint links spawn --userscript view_in_mpv {hint-url}',
-    ';M': 'spawn kitty -e yt-dlp --embed-metadata \
+    "pw": "spawn -u qute-keepassxc --key ABC1234",
+    ",m": "spawn -u -m view_in_mpv {url}",
+    ",M": "hint links spawn -u -m view_in_mpv {hint-url}",
+    "pf": "spawn --userscript password_fill",
+    "mf": "hint all spawn -d firefox {hint-url}",
+    ";M": 'spawn kitty -e yt-dlp --embed-metadata \
         --embed-thumbnail -i -o "~/Videos/%(title)s.%(ext)s" -f mp4 \
         --sponsorblock-remove all --write-sub --sub-lang en \
         --convert-subs vtt {url}',
-    'pf': 'spawn --userscript password_fill',
-    'mf': 'hint all spawn -d firefox {hint-url}',
-    '<f12>': 'devtools bottom',
 }
 for key, bind in bindings.items():
     config.bind(key, bind)
@@ -171,17 +181,12 @@ c.content.cookies.store = True
 c.content.webrtc_ip_handling_policy = "default-public-interface-only"
 
 
-config.set('content.cookies.accept', 'all', 'chrome-devtools://*')
-config.set('content.cookies.accept', 'all', 'devtools://*')
-config.set('content.headers.accept_language', '', 'https://matchmaker.krunker.io/*')
-# config.set('content.headers.user_agent', 'Mozilla/5.0 ({os_info}) AppleWebKit/{webkit_version} (KHTML, like Gecko) {upstream_browser_key}/{upstream_browser_version} Safari/{webkit_version}', 'https://web.whatsapp.com/')
-# config.set('content.headers.user_agent', 'Mozilla/5.0 ({os_info}; rv:90.0) Gecko/20100101 Firefox/90.0', 'https://accounts.google.com/*')
-# config.set('content.headers.user_agent', 'Mozilla/5.0 ({os_info}) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99 Safari/537.36', 'https://*.slack.com/*')
-# config.set('content.headers.user_agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:54.0) Gecko/20100101 Firefox/54.0')
-# config.set('content.headers.user_agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3')
-config.set('content.images', True, 'chrome-devtools://*')
-config.set('content.images', True, 'devtools://*')
-config.set('content.javascript.enabled', True, 'chrome-devtools://*')
-config.set('content.javascript.enabled', True, 'devtools://*')
-config.set('content.javascript.enabled', True, 'chrome://*/*')
-config.set('content.javascript.enabled', True, 'qute://*/*')
+config.set("content.cookies.accept", "all", "chrome-devtools://*")
+config.set("content.cookies.accept", "all", "devtools://*")
+config.set("content.headers.accept_language", "", "https://matchmaker.krunker.io/*")
+config.set("content.images", True, "chrome-devtools://*")
+config.set("content.images", True, "devtools://*")
+config.set("content.javascript.enabled", True, "chrome-devtools://*")
+config.set("content.javascript.enabled", True, "devtools://*")
+config.set("content.javascript.enabled", True, "chrome://*/*")
+config.set("content.javascript.enabled", True, "qute://*/*")
